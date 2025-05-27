0cadca829a2e7042bcb452718cc74f2a198d72f4
Mark Kabugi <kabug222@gmail.com>
2025-05-27 21:33:18 +0300

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,9 @@
 const a = 2;
-if (a = 0) {
-    console.log("No argument")
-} else if (a = 1) {
-    console.log("Argument found")
+
+if (a === 0) {
+  console.log("No argument");
+} else if (a === 1) {
+  console.log("Argument found");
 } else {
-    console.log("Arguments found")
+  console.log("Arguments found");
 }
